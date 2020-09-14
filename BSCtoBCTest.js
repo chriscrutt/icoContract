93abd70ff60b66ed259420749780e11830cb45aa
Chris <chriscrutt@gmail.com>
2020-09-13 20:00:06 -0700

    document.ready -> window.load

--- BSCtoBCTest.js
+++ BSCtoBCTest.js
@@ -6,7 +6,7 @@ function toHexString(byteArray) {
 
 var myEther;
 
-$(document).ready(function () {
+$(window).load(function () {
 
     if (web3.currentProvider) {
         console.log(web3.currentProvider);

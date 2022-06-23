c86636ac8f943e99bb86a18577d83c5771c7f748
Chris <chriscrutt@gmail.com>
2022-06-22 18:07:38 -0700

    send dif tokens drop down

--- main-copy.js
+++ main-copy.js
@@ -91,6 +91,40 @@ sendEthButton.addEventListener("click", async () => {
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+function dropIt() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches("#dropbtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+};
+
+const options = document.querySelector(".options");
+
+function doSomething(a) {
+    document.querySelector("#dropbtn").innerHTML = a.innerText;
+}
+
+let tokens = {
+    BAL: "0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7",
+    DAI: "0x04DF6e4121c27713ED22341E7c7Df330F56f289B",
+    GUSD: "0x22ee6c3B011fACC530dd01fe94C58919344d6Db5",
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 // A Human-Readable ABI; for interacting with the contract, we
 // must include any fragment we wish to use
 const abi = [
@@ -106,33 +140,46 @@ const abi = [
     "event Transfer(address indexed from, address indexed to, uint amount)",
 ];
 
-// This can be an address or an ENS name
-const address = "0x04DF6e4121c27713ED22341E7c7Df330F56f289B";
+function getTokenInfo(write) {
+    try {
+        // This can be an address or an ENS name
+        let address = tokens[document.querySelector("#dropbtn").innerHTML];
 
-// Read-Only; By connecting to a Provider, allows:
-// - Any constant function
-// - Querying Filters
-// - Populating Unsigned Transactions for non-constant methods
-// - Estimating Gas for non-constant (as an anonymous sender)
-// - Static Calling non-constant methods (as anonymous sender)
-const erc20 = new ethers.Contract(address, abi, provider);
+        if (!write) {
+            // Read-Only; By connecting to a Provider, allows:
+            // - Any constant function
+            // - Querying Filters
+            // - Populating Unsigned Transactions for non-constant methods
+            // - Estimating Gas for non-constant (as an anonymous sender)
+            // - Static Calling non-constant methods (as anonymous sender)
+            return new ethers.Contract(address, abi, provider);
+        } else {
+            // Read-Write; By connecting to a Signer, allows:
+            // - Everything from Read-Only (except as Signer, not anonymous)
+            // - Sending transactions for non-constant functions
+            return new ethers.Contract(address, abi, signer);
+        }
 
-// Read-Write; By connecting to a Signer, allows:
-// - Everything from Read-Only (except as Signer, not anonymous)
-// - Sending transactions for non-constant functions
-const erc20_rw = new ethers.Contract(address, abi, signer);
+    } catch (err) {
+        document.getElementById("dropbtn").innerHTML = "select a coin";
+        document.getElementById("dropbtn").style.color = "red";
+        console.error;
+    }
+}
 
-const daiBalanceButton = document.querySelector("#daiBalanceButton");
+const tokenBalanceButton = document.querySelector("#tokenBalanceButton");
 
-daiBalanceButton.addEventListener("click", async () => {
+tokenBalanceButton.addEventListener("click", async () => {
+    const erc20 = getTokenInfo(false);
     const num = erc20
         .balanceOf(signer.getAddress())
         .then(async (p) => {
             yo = await p;
-            document.querySelector("#daiBalance").innerHTML =
-                ethers.utils.formatUnits(p._hex, await erc20.decimals()) +
-                " DAI";
-            document.querySelector("#daiBalance").style.display = "block";
+            document.querySelector("#tokenBalance").innerHTML =
+                ethers.utils.formatUnits(p._hex, await erc20.decimals()) + " " + 
+                document.querySelector("#dropbtn").innerHTML;
+            document.querySelector("#tokenBalance").style.display = "block";
+            document.getElementById("dropbtn").style.color = "white";
         })
         .catch((error) => console.error);
 });
@@ -143,22 +190,23 @@ daiBalanceButton.addEventListener("click", async () => {
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-const sendDaiButton = document.querySelector("#sendDaiButton");
+const sendTokenButton = document.querySelector("#sendTokenButton");
 
 //Sending Dai to an address
-sendDaiButton.addEventListener("click", async () => {
+sendTokenButton.addEventListener("click", async () => {
+    const erc20_rw = getTokenInfo(true);
     const tx = await erc20_rw
         .transfer(
-            await provider.resolveName(document.querySelector("#toDai").value),
+            await provider.resolveName(document.querySelector("#toToken").value),
             ethers.utils.parseUnits(
-                document.querySelector("#dai").value,
-                await erc20.decimals()
+                document.querySelector("#token").value,
+                await erc20_rw.decimals()
             )
         )
         .then((txHash) => {
-            document.querySelector("#sendDai").innerHTML = txHash.hash;
-            document.querySelector("#sendDai").style.display = "block";
-            document.querySelector("#sendDai").href += txHash.hash;
+            document.querySelector("#sendToken").innerHTML = txHash.hash;
+            document.querySelector("#sendToken").style.display = "block";
+            document.querySelector("#sendToken").href += txHash.hash;
         })
         .catch((error) => console.error);
 });
